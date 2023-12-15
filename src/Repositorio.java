ed535d11e9e673e702e02ad5e87521ad4a555271
Ricardo França <ricardofs547@live.com>
2023-12-15 06:24:27 -0300

    repositorio populado

--- src/Repositorio.java
+++ src/Repositorio.java
@@ -7,8 +7,18 @@ public class Repositorio {
 
     
     private Repositorio() {
+        Aeronave a1 = new Aeronave("aerotaw1", 1);
+        Aeronave a2 = new Aeronave("aerotaw2", 2);
+        Aeronave a3 = new Aeronave("aerotaw3", 1);
         
+        Voo v1 = new Voo("v1", "taw", "ham", "12/12/2012", "00:00", "03:00", "p1", a1);
+        Voo v2 = new Voo("v2", "ham", "taw", "09/12/2002", "12:02", "15:00", "p2", a2);
+        Voo v3 = new Voo("v3", "balbino", "paula", "08/07/2014", "00:00", "02:00", "p3", a3);
+
         this.voos = new ArrayList<Voo>();
+        this.voos.add(v1);
+        this.voos.add(v2);
+        this.voos.add(v3);
         
     }
     
